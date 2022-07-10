ca0517453e188de7239b08be501ce4b548cbe4e5
JulioNunesDev <julionunes999@gmail.com>
2022-07-10 00:46:03 -0300

    selenium aprendendo algumas ferramentas

---
+++ Selenium_teste_.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+options = Options()
+options.add_argument('window-size=1280,800')
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+navegador = webdriver.Chrome(options=options)
+
+navegador.get('https://olhardigital.com.br/')
+
+
+poupNot = navegador.find_element(By.ID, 'onesignal-slidedown-cancel-button')
+poupNot.click()
+sleep(1)
+elemement = navegador.find_element(By.CLASS_NAME, 'search-button')
+elemement.click()
+
+inputSearch = navegador.find_element(By.ID, 'mainSearchInput')
+inputSearch.send_keys('Tecnologia')
+inputSearch.submit()
+
+
+
