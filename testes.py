3263f6572a1ab30f4e735f0f7cd4194bfdd91d12
JulioNunesDev <julionunes999@gmail.com>
2022-07-10 00:46:17 -0300

    testando na pratica

---
+++ testes.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+
+navegador = webdriver.Chrome()
+
+navegador.get('https://www.google.com.br')
+
+inputGoogle = navegador.find_element(By.TAG_NAME, 'input')
+button = navegador.find_element(By.LINK_TEXT, 'Fazer login')
+button.click()
+inputOk = navegador.find_element(By.TAG_NAME, 'input')
+inputOk.send_keys('Your E-mail here!')
+
+buttonProx = navegador.find_element(By.ID, 'identifierNext')
+buttonProx.click()
+sleep(3)
+inputPass = navegador.find_element(By.NAME, 'password')
+
+
+inputPass.send_keys('Your password here!')
+buttonSenha = navegador.find_element(By.ID, 'passwordNext')
+buttonSenha.click()
+
